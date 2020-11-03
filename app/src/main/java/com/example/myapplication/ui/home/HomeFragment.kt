32074b9c6d08843ae03570b5671586bf62335553
Diego Criado <dcriado.dive@gmail.com>
2020-11-03 13:41:39 -0300

    pre offer dashboard card and auction dashboard card

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -9,8 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.auction_dashboard_card.*
+import kotlinx.android.synthetic.main.pre_offer_auction_dashboar_card.*
 
 class HomeFragment : Fragment() {
+
+    lateinit var auctionDashboardCard: View
+    lateinit var preOfferDashboardCard : View
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -21,10 +26,25 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+        auctionDashboardCard = inflater.inflate(R.layout.auction_dashboard_card, container, false)
+        preOfferDashboardCard = inflater.inflate(R.layout.pre_offer_auction_dashboar_card, container, false)
        // val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
            // textView.text = it
         })
         return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        show_auction_list_txt.setOnClickListener {
+
+        }
+
+        show_pre_offer_list_txt.setOnClickListener {
+
+        }
     }
 }
